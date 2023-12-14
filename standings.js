acee0c4298bb19318781e60912d2fdf77d631c3e
scottCheung <scottcx2@gmail.com>
2023-12-14 10:53:57 -0500

    production url

--- standings.js
+++ standings.js
@@ -174,7 +174,7 @@ footballStats.init = async () =>{
      try {
       const apiKeyName = "apiKey3";
       const response = await fetch(
-        `http://localhost:8888/.netlify/functions/getApiKey?key=${apiKeyName}`
+        `https://worldcup-app.netlify.app/.netlify/functions/getApiKey?key=${apiKeyName}`
       );
       if (!response.ok) {
         throw new Error("Failed to fetch API key");
